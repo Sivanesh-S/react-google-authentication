9dce2cbfef807be5b424c24cfbb723ecd4d2bbdc
Sivanesh Shanmugam <sivaneshraina@gmail.com>
2020-06-27 01:19:10 +0530

    formatted

--- src/App.js
+++ src/App.js
@@ -10,13 +10,17 @@ function App() {
     <div className="App">
       <h2>The Components way</h2>
       <Login />
+      <br />
       <Logout />
       <h2>The Hooks way</h2>
       <LoginHooks />
       <LogoutHooks />
+      <br />
+      If it does helped you feel free to star{' '}
       <a href="https://github.com/Sivanesh-S/react-google-authentication">
         Github Repo
-      </a>
+      </a>{' '}
+      😉
     </div>
   );
 }
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -17,7 +17,6 @@ function Logout() {
         buttonText="Logout"
         onLogoutSuccess={onSuccess}
       ></GoogleLogout>
-      ;
     </div>
   );
 }
