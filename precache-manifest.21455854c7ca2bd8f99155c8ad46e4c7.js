0c25793ce202a79e6b52839eebbcf5ce8846a7d1
Sivanesh Shanmugam <sivaneshraina@gmail.com>
2020-06-26 23:37:58 +0530

    Updates

---
+++ precache-manifest.21455854c7ca2bd8f99155c8ad46e4c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da65066d71c710cba8609419c9ba4b95",
+    "url": "/react-google-authentication/index.html"
+  },
+  {
+    "revision": "c1c69c9a7df59d80f89e",
+    "url": "/react-google-authentication/static/css/main.f072c70a.chunk.css"
+  },
+  {
+    "revision": "a8f738033b3241a44756",
+    "url": "/react-google-authentication/static/js/2.c988052e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-google-authentication/static/js/2.c988052e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c69c9a7df59d80f89e",
+    "url": "/react-google-authentication/static/js/main.bfd6ce1f.chunk.js"
+  },
+  {
+    "revision": "a5d82ddcb42d9d67d879",
+    "url": "/react-google-authentication/static/js/runtime-main.406eb9a3.js"
+  }
+]);
